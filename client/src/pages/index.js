773384807a4e448fd684a20ecc6c6923d48aa27e
FredSchuck <fredschuck@gmail.com>
2024-02-29 17:10:39 -0500

    Added Signup page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-// Description: Export all components from this file to be imported in App.jsx. This is a way to import all components from a single file instead of importing each component individually.
+// Description: Export all pages from this file to be imported in App.jsx. This is a way to import all pages from a single file instead of importing each component individually.
 
 import Home from "./Home";
 import Signup from "./Signup";
