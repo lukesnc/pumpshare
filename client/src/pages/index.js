db297c0dd3c123f03dfa25e26041d02d6dc83b24
FredSchuck <fredschuck@gmail.com>
2024-02-29 13:53:43 -0500

    Initial commit

---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+// Description: Export all components from this file to be imported in App.jsx. This is a way to import all components from a single file instead of importing each component individually.
+
+import Home from "./Home";
+import Signup from "./Signup";
+
+export {
+Home,
+Signup
+};
