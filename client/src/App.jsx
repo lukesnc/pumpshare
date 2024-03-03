f39e02e1697c0d064f70ab3cda2be07d6e536c5f
FredSchuck <fredschuck@gmail.com>
2024-03-02 23:27:26 -0500

    Removed unused import 'styles'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 // import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import styles from './style'
 
 // Pages and Components
 import { Home, Signup } from './pages'
