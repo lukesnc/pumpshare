c8b6371949fe923955a2c184c04c7053a9b424d2
FredSchuck <fredschuck@gmail.com>
2024-03-02 21:43:10 -0500

    Added proxy to allow for requests to server

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Allows for the client to make requests to the server
+      '/api': 'http://localhost:3000',
+    }
+  }
 })
