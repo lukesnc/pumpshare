6e7ccb0f70ea86cbb91555bb868576b433ebaa6d
FredSchuck <fredschuck@gmail.com>
2024-03-18 19:15:59 -0400

    Updated user schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 const Schema = require('mongoose').Schema;
 
 const userSchema = new Schema({
+    firstName: { type: String, required: [true, 'First Name is required'], trim: true, minlength: 1 },
+    lastName: { type: String, required: [true, 'Last Name is required'], trim: true, minlength: 1 },
     email: { type: String, required: [true, 'Email is required'], unique: true, lowercase: true, trim: true, minlength: 3 },
-    password: { type: String, required: [true, 'Password is required'], trim: true, minlength: 8 }
+    password: { type: String, required: [true, 'Password is required'], trim: true, minlength: 8 },
+    username: { type: String, trim: true, minlength: 1 },
+    displayName: { type: String, trim: true, minlength: 1 },
+    avatar: { type: String, trim: true },
+    bio: { type: String, trim: true },
+    posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    followers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    following: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    likedPosts: [{ type: Schema.Types.ObjectId, ref: 'Post' }]
 }, { timestamps: true });
 
 // Replace if-else with 'return bcrypt.compare(inputPassword, user.password)' when bcrypt is introduced
