9fff8bacf706edb671f5579a0f8f52539d79438d
FredSchuck <fredschuck@gmail.com>
2024-03-18 19:15:42 -0400

    Added posts and starter controller methods

---
+++ server/routes/posts.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/postsController');
+
+// GET all users
+router.get('/', controller.getAllPosts);
+
+// GET one post
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET one post'});
+});
+// POST a post
+router.post('/post', controller.postPost);
+
+module.exports = router;
