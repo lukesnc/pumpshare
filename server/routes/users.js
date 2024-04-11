e7271364426a399fff02733e59ce197c7242d21a
FredSchuck <fredschuck@gmail.com>
2024-04-11 19:07:38 -0400

    Added checkUsername from validator middleware

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/usersController');
+const controller = require("../controllers/usersController");
+const checkUsername = require("../middleware/validator");
 
 // GET all users
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all users'});
+router.get("/", (req, res) => {
+  res.json({ mssg: "GET all users" });
 });
 
 // GET one user
-router.get('/:username', controller.getProfile);
+router.get("/:username", checkUsername, controller.getProfile);
 
+router.post("/login", controller.loginUser);
 
-router.post('/login', controller.loginUser);
-
-router.post('/register', controller.registerUser);
+router.post("/register", controller.registerUser);
 
 module.exports = router;
