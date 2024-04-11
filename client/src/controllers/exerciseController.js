447b1124206da5699fbdf7c57ce7fef0d97b2f8f
FredSchuck <fredschuck@gmail.com>
2024-04-11 19:01:23 -0400

    Update

--- client/src/controllers/exerciseController.js
+++ client/src/controllers/exerciseController.js
@@ -1,47 +1,45 @@
+const createExercise = async (name, attr) => {
+  if (!name || !attr) {
+    throw Error("All fields are required");
+  }
 
-const createExercise = async (name, attr) =>{
-    if (!name || !attr) {
-        throw Error('All fields are required');
-    }
+  const res = await fetch("/api/exercises/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, attr }),
+  });
 
-    const res = await fetch('/api/exercises/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, attr }),
-    });
-    
-    // data is the response from the server
-    const data = await res.json();
-    if (!res.ok) {
-        throw Error(data.error);
-    }
+  // data is the response from the server
+  const data = await res.json();
+  if (!res.ok) {
+    throw Error(data.error);
+  }
 
-    return data; 
+  return data;
 };
 
-const logExercise = async (id,date,sets,reps,weight, about) =>{
-    if (!date || !sets || !reps || !weight || !about) {
-        console.log(date);
-        throw Error('All fields are required');
-    }
+const logExercise = async (id, date, sets, reps, weight, about) => {
+  if (!date || !sets || !reps || !weight || !about) {
+    throw Error("All fields are required");
+  }
 
-    const res = await fetch('/api/exercises/log', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ date,sets,reps,weight, about }),
-    });
-    
-    // data is the response from the server
-    const data = await res.json();
-    if (!res.ok) {
-        throw Error(data.error);
-    }
+  const res = await fetch("/api/exercises/log", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ date, sets, reps, weight, about }),
+  });
 
-    return data;
+  // data is the response from the server
+  const data = await res.json();
+  if (!res.ok) {
+    throw Error(data.error);
+  }
+
+  return data;
 };
 
-export { createExercise, logExercise }
+export { createExercise, logExercise };
