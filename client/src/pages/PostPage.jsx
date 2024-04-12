8ea16b51845ff6c61a4965ceff52ed366ba9e16e
FredSchuck <fredschuck@gmail.com>
2024-04-12 01:08:59 -0400

    Fixed styling and added comments section and 'add comment' button

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -26,22 +26,43 @@ const PostPage = () => {
   }, [id]);
 
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center">
-      <div className="max-w-md w-full p-8 mx-4 bg-white rounded-lg shadow-md mb-auto mt-[100px] text-center">
-        <h2 className="form-title">Post Page</h2>
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      {/* Post */}
+      <div className="mt-12 w-full">
+        <div className="">
+          {post.likes && ( // Check if post data exists
+            <div
+              className={`${styles.postCard} flex flex-col border-t-2 border-gray-100`}
+            >
+              <PostCard
+                id={post._id}
+                userId={post.user}
+                content={post.content}
+                comments={post.comments}
+                likes={post.likes}
+                timestamp={post.timestamp}
+              />
+            </div>
+          )}
+        </div>
       </div>
-      {/* <div
-        className={`${styles.postCard} flex flex-col border-t-2 border-gray-100`}
-      >
-        <PostCard
-          id={post._id}
-          userId={post.user}
-          content={post.content}
-          comments={post.comments}
-          likes={post.likes}
-          timestamp={post.timestamp}
-        />
-      </div> */}
+      <div className="flex flex-col text-center  w-full">
+        {/* Add Comment */}
+        <div className="w-full bg-emeraldMist">
+          <button className="w-full max-w-sm bg-emeraldMist text-white font-medium py-2 px-4">
+            Add Comment
+          </button>
+        </div>
+        {/* Comments */}
+        <div>
+          <h3 className="text-gray-400 font-semibold my-5">Comments</h3>
+          <div className="bg-white py-6 border-t-2 border-gray-100">
+            <p className="text-primary text-[14px] mx-2">
+              This is a fake comment. Replace with actual comments later.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
