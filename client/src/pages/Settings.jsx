cb73bc536a76efc594b5e2f93e9cfe19e74042bb
FredSchuck <fredschuck@gmail.com>
2024-04-18 01:32:39 -0400

    Added useContext to change user context, delete handler, and password verification to delete

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,5 +1,12 @@
-import { useState, useEffect } from "react";
-import { getMe, updateUserData } from "../controllers/userController";
+import { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  getMe,
+  updateUserData,
+  deleteUser,
+  verifyPassword,
+} from "../controllers/userController";
+import { UserContext } from "../contexts/userContext";
 
 const Settings = () => {
   const imagePlaceholder = "../images/avatar.png"; // To be removed later
@@ -12,6 +19,10 @@ const Settings = () => {
   const [image, setImage] = useState(null);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const { setUser } = useContext(UserContext);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -62,6 +73,31 @@ const Settings = () => {
     // }
   };
 
+  const handleDelete = async () => {
+    if (!password) {
+      setError("Please enter your password to delete your account");
+      return;
+    }
+    const verifyPasswordResponse = await verifyPassword(password);
+    const data = await verifyPasswordResponse.json();
+    if (!verifyPasswordResponse.ok) {
+      setError(data.message);
+      return;
+    }
+
+    const res = await deleteUser();
+    if (!res.ok) {
+      setError(res.error);
+      return;
+    } else {
+      setUser({ email: null, posts: [] });
+      localStorage.removeItem("email");
+      localStorage.removeItem("token");
+      localStorage.removeItem("username");
+      navigate("/");
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const userData = { firstName, lastName, email, password, image };
@@ -95,103 +131,155 @@ const Settings = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center">
-      <div className="max-w-md w-full p-8 mx-4 bg-white rounded-lg shadow-md mb-auto mt-[100px]">
-        <h2 className="form-title mb-4">Profile Settings</h2>
-        <div className="flex justify-center">
-          <label htmlFor="image">
-            <img
-              src={imagePlaceholder}
-              alt="avatar"
-              className="w-[100px] mb-4"
-            />
-          </label>
-          <input
-            type="file"
-            id="image"
-            name="image"
-            className="hidden"
-            onChange={handleImageChange}
-          />
-          <div className="w-7 h-7 rounded-full border border-gray-500 bg-white absolute top-[255px] left-[210px] flex justify-center items-center">
-            <i className="fa-solid fa-pen fa-sm text-gray-500"></i>
+    <>
+      {isDeleting ? (
+        <div className="min-h-screen bg-gray-100 flex justify-center">
+          <div className="max-w-md w-full pt-8 px-8 pb-4 mx-4 bg-white rounded-lg shadow-md mb-auto mt-[100px]">
+            <h2 className="input-label text-center text-xl">
+              Enter your password to delete your account
+            </h2>
+            <div className="mb-12 mt-12">
+              <label htmlFor="password" className="input-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="input pl-2"
+                value={password}
+                onChange={handleInputChange}
+              />
+              {error && <p className="text-red-500 text-center">{error}</p>}
+            </div>
+            <div className="flex justify-center mt-4">
+              <button
+                className="bg-emeraldMist text-white px-4 py-2 rounded mr-4 w-max"
+                onClick={() => {
+                  setIsDeleting(false);
+                  setError(null);
+                  setPassword("");
+                }}
+              >
+                Cancel
+              </button>
+              <button
+                className="bg-red-700 text-white px-4 py-2 rounded"
+                onClick={handleDelete}
+              >
+                Delete
+              </button>
+            </div>
           </div>
         </div>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="firstName" className="input-label">
-              First Name
-            </label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              className="input pl-2"
-              value={firstName}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="lastName" className="input-label">
-              Last Name
-            </label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              className="input pl-2"
-              value={lastName}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="input-label">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="input pl-2"
-              value={email}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="input-label">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="input pl-2"
-              value={password}
-              onChange={handleInputChange}
-            />
-          </div>
+      ) : (
+        <div className="min-h-screen bg-gray-100 flex justify-center">
+          <div className="max-w-md w-full pt-8 px-8 pb-4 mx-4 bg-white rounded-lg shadow-md mb-auto mt-[100px]">
+            <h2 className="form-title mb-4">Profile Settings</h2>
+            <div className="flex justify-center">
+              <label htmlFor="image">
+                <img
+                  src={imagePlaceholder}
+                  alt="avatar"
+                  className="w-[100px] mb-4"
+                />
+              </label>
+              <input
+                type="file"
+                id="image"
+                name="image"
+                className="hidden"
+                onChange={handleImageChange}
+              />
+              <div className="w-7 h-7 rounded-full border border-gray-500 bg-white absolute top-[255px] left-[210px] flex justify-center items-center">
+                <i className="fa-solid fa-pen fa-sm text-gray-500"></i>
+              </div>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label htmlFor="firstName" className="input-label">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  className="input pl-2"
+                  value={firstName}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="lastName" className="input-label">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  className="input pl-2"
+                  value={lastName}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="email" className="input-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="input pl-2"
+                  value={email}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="input-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="input pl-2"
+                  value={password}
+                  onChange={handleInputChange}
+                />
+              </div>
 
-          <div className="mb-4">
-            <label htmlFor="confirmPassword" className="input-label">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              className="input pl-2"
-              value={confirmPassword}
-              onChange={handleInputChange}
-            />
+              <div className="mb-4">
+                <label htmlFor="confirmPassword" className="input-label">
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  className="input pl-2"
+                  value={confirmPassword}
+                  onChange={handleInputChange}
+                />
+              </div>
+              {error && <p className="text-red-500 text-center">{error}</p>}
+              {success && (
+                <p className="text-green-500 text-center">{success}</p>
+              )}
+              <button type="submit" className="form-btn">
+                Update
+              </button>
+              <a
+                className="text-red-700 mt-4 flex justify-end text-[12px]"
+                onClick={() => setIsDeleting(true)}
+              >
+                Delete Account
+              </a>
+            </form>
           </div>
-          {error && <p className="text-red-500 text-center">{error}</p>}
-          {success && <p className="text-green-500 text-center">{success}</p>}
-          <button type="submit" className="form-btn">
-            Update
-          </button>
-        </form>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
