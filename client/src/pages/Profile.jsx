341aff8fc01bda6eb7ebeac0a2ba7bad3008a993
FredSchuck <fredschuck@gmail.com>
2024-04-18 22:14:55 -0400

    Added following functionality and numbers of followers/following

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,22 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../contexts/userContext";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { PostCard } from "../components";
+import { PostCard, Alert } from "../components";
 import styles from "../style";
+import { followUser } from "../controllers/userController";
 
 const Profile = () => {
   const { username } = useParams();
   const [userData, setUserData] = useState(null);
   const [posts, setPosts] = useState([]);
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [followError, setFollowError] = useState(null);
+  const [userExists, setUserExists] = useState(true);
+
+  const [followers, setFollowers] = useState([]);
+  const [following, setFollowing] = useState([]);
+
+  const { user } = useContext(UserContext);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const response = await fetch(`/api/users/${username}`);
+        const data = await response.json();
+        console.log(data);
         if (!response.ok) {
+          if (response.status === 404) {
+            setUserExists(false);
+          }
           throw new Error("Error fetching user data");
         }
-        const data = await response.json();
+        setFollowers(data.followers);
+        setFollowing(data.following);
         setUserData(data);
 
         if (data.posts.length > 0) {
@@ -35,62 +51,129 @@ const Profile = () => {
     fetchUserData();
   }, [username]);
 
+  useEffect(() => {
+    const fetchFollowing = async () => {
+      try {
+        const response = await fetch(`/api/users/${user.username}/following`);
+        if (!response.ok) {
+          throw new Error("Error fetching followers");
+        }
+        const data = await response.json();
+        if (data.usersList.length > 0) {
+          const followingList = data.usersList.map((user) => user.username);
+          setIsFollowing(followingList.includes(username));
+        }
+      } catch (error) {}
+    };
+
+    fetchFollowing();
+  }, [username]);
+
   const firstName = userData ? userData.firstName : "";
   const lastName = userData ? userData.lastName : "";
 
+  const handleFollow = async () => {
+    try {
+      const res = await followUser(username);
+      const data = await res.json();
+      if (!res.ok) {
+        throw new Error(data.error);
+      }
+      setIsFollowing(true);
+    } catch (error) {
+      console.error("Error following user:", error);
+    }
+  };
+
   return (
     <div>
-      {/* Profile Info */}
-      <div className="flex flex-col min-h-screen bg-gray-100 items-center">
-        <div className="flex flex-col items-center mt-[5rem] w-full">
-          <div className="">
-            <img src="./images/avatar.png" alt="avatar" className="w-[10rem]" />
+      {userExists ? (
+        <div className="flex flex-col min-h-screen bg-gray-100 items-center">
+          <div className="flex flex-col items-center mt-[5rem] w-full">
+            <div className="">
+              <img
+                src="./images/avatar.png"
+                alt="avatar"
+                className="w-[10rem]"
+              />
+            </div>
+            <div className="flex flex-col items-center w-full">
+              <h1 className="text-[24px] font-semibold mt-2">
+                {firstName} {lastName}
+              </h1>
+              <p className="text-gray-400">@{username}</p>
+              <div className="flex mt-4 justify-evenly w-full">
+                {/* Follow Button */}
+                {username === user.username || isFollowing ? (
+                  ""
+                ) : (
+                  <button
+                    className="bg-tealGreen text-white px-4 py-2 rounded-md"
+                    onClick={handleFollow}
+                  >
+                    <i className="fa-solid fa-plus mr-3"></i>Follow
+                  </button>
+                )}
+                {followError && <Alert message={followError} type="error" />}
+              </div>
+              <div className="flex mt-4 justify-evenly w-full">
+                <Link to={`/${username}/followers`} className="text-tealGreen">
+                  {followers.length} Followers
+                </Link>
+                <Link to={`/${username}/following`} className="text-tealGreen">
+                  {following.length} Following
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-col items-center w-full">
-            <h1 className="text-[24px] font-semibold mt-2">
-              {firstName} {lastName}
-            </h1>
-            <p className="text-gray-400">@{username}</p>
-            <div className="flex mt-4 justify-evenly w-full">
-              <Link to={`/${username}/followers`} className="text-tealGreen">
-                Followers
-              </Link>
-              <Link to={`/${username}/following`} className="text-tealGreen">
-                Following
-              </Link>
+
+          {/* Posts */}
+          <div className="mt-5 w-full">
+            <div className="">
+              {/* >>> add a loading here (maybe in with posts.length?) <<< */}
+              {posts.length === 0 && (
+                <div className="flex mt-4 bg-white h-[100px]">
+                  <p className="text-gray-400 mx-auto my-auto font-[16px]">
+                    No posts available
+                  </p>
+                </div>
+              )}
+              {posts.map((post) => (
+                <div
+                  className={`${styles.postCard} flex flex-col border-t-2 border-gray-100`}
+                >
+                  <PostCard
+                    key={post.id}
+                    id={post._id}
+                    userId={post.user}
+                    content={post.content}
+                    comments={post.comments}
+                    likes={post.likes}
+                    timestamp={post.timestamp}
+                  />
+                </div>
+              ))}
             </div>
           </div>
         </div>
-
-        {/* Posts */}
-        <div className="mt-5 w-full">
-          <div className="">
-            {/* >>> add a loading here (maybe in with posts.length?) <<< */}
-            {posts.length === 0 && (
-              <div className="flex mt-4 bg-white h-[100px]">
-                <p className="text-gray-400 mx-auto my-auto font-[16px]">
-                  No posts available
-                </p>
-              </div>
-            )}
-            {posts.map((post) => (
-              <div
-                className={`${styles.postCard} flex flex-col border-t-2 border-gray-100`}
-              >
-                <PostCard
-                  key={post.id}
-                  id={post._id}
-                  userId={post.user}
-                  content={post.content}
-                  comments={post.comments}
-                  likes={post.likes}
-                  timestamp={post.timestamp}
-                />
-              </div>
-            ))}
+      ) : (
+        <div className="flex flex-col min-h-screen bg-gray-100 items-center">
+          <div className="flex flex-col items-center mt-[5rem] w-full">
+            <div className="">
+              <img
+                src="./images/avatar.png"
+                alt="avatar"
+                className="w-[10rem]"
+              />
+            </div>
+            <div className="flex flex-col items-center w-full">
+              <h1 className="text-[24px] font-semibold mt-2 mx-8 text-center">
+                The user @{username} does not exist
+              </h1>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
