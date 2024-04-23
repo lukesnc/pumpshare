6781868f202a29a6a2800627a0f060a2ad1cd305
lukesnc <lukeasimone@gmail.com>
2024-04-23 19:22:59 -0400

    add getAllExercises to exercise controller

--- server/controllers/exercisesController.js
+++ server/controllers/exercisesController.js
@@ -1,44 +1,48 @@
-const Exercise = require('../models/exercise');
+const Exercise = require("../models/exercise");
 
+exports.createExercise = async (req, res) => {
+  const { name, attr } = req.body;
+  if (!name || !attr) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
 
-
-exports.createExercise = async (req,res) =>{
-    const {name, attr} = req.body;
-    if (!name || !attr){
-        return res.status(400).json({error: 'All fields are required'});
-    }
-
-    const exercise = new Exercise({name, attr});
-    exercise.save()
-    .then(()=>{
-        res.status(200).json(exercise);
+  const exercise = new Exercise({ name, attr });
+  exercise
+    .save()
+    .then(() => {
+      res.status(200).json(exercise);
     })
-    .catch(error => res.status(400).json({error: error.message}));    
-}
+    .catch((error) => res.status(400).json({ error: error.message }));
+};
 
-exports.logExercise = async (req,res) =>{
-    const { id } = req.params;
-    
-    const {date, sets, reps, weight,about} = req.body;
-    if (!date || !sets || !reps || !weight || !about){
-        return res.status(400).json({error: 'All fields are required'});
-    }
-    Exercise.findOneAndUpdate(
-        id,
-        {
-            date:date,
-            sets:sets,
-            reps:reps,
-            weight:weight,
-            about:about
-        }
-    )
-    .then((exercise) =>{
-        res.status(200).json({exercise});
-        //res.redirect(`/:${exercise._id}`);
+exports.getAllExercises = async (req, res) => {
+  Exercise.find()
+    .then((posts) => {
+      res.status(200).json(posts);
     })
-    .catch(error => res.status(500).json({error: error.message}));
-    
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: "Error fetching exercises" });
+    });
+};
 
-    
-}
+exports.logExercise = async (req, res) => {
+  const { id } = req.params;
+
+  const { date, sets, reps, weight, about } = req.body;
+  if (!date || !sets || !reps || !weight || !about) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+  Exercise.findOneAndUpdate(id, {
+    date: date,
+    sets: sets,
+    reps: reps,
+    weight: weight,
+    about: about,
+  })
+    .then((exercise) => {
+      res.status(200).json({ exercise });
+      //res.redirect(`/:${exercise._id}`);
+    })
+    .catch((error) => res.status(500).json({ error: error.message }));
+};
--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -1,30 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/exercisesController');
+const controller = require("../controllers/exercisesController");
 
 // GET all exercises
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all exercises'});
-});
+router.get("/", controller.getAllExercises);
 
 // GET one exercise
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET one exercise'});
+router.get("/:id", (req, res) => {
+  res.json({ mssg: "GET one exercise" });
 });
 
 // POST one exercise
-router.post('/', controller.createExercise);
+router.post("/", controller.createExercise);
 
-router.post('/log', controller.logExercise);
+router.post("/log", controller.logExercise);
 
 // DELETE one exercise
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE one exercise'});
+router.delete("/:id", (req, res) => {
+  res.json({ mssg: "DELETE one exercise" });
 });
 
 // UPDATE one exercise
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE one exercise'});
+router.patch("/:id", (req, res) => {
+  res.json({ mssg: "UPDATE one exercise" });
 });
 
 module.exports = router;
