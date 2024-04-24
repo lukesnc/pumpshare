0e3bf6648666b5b0a5dd3711c82cefb322549f8d
lukesnc <lukeasimone@gmail.com>
2024-04-23 22:32:40 -0400

    create workouts controller + add to server

--- server/server.js
+++ server/server.js
@@ -1,41 +1,44 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const MongoStore = require('connect-mongo'); // Session store for MongoDB
-const mainRoutes = require('./routes/main');
-const exercisesRoutes = require('./routes/exercises');
-const usersRoutes = require('./routes/users');
-const postsRoutes = require('./routes/posts');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo"); // Session store for MongoDB
+const mainRoutes = require("./routes/main");
+const exercisesRoutes = require("./routes/exercises");
+const usersRoutes = require("./routes/users");
+const postsRoutes = require("./routes/posts");
+const workoutsRoutes = require("./routes/workouts");
 
 // Express App
 const app = express();
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 3000;
 
 // Database Connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('Connected to database successfully');
-        app. listen(process.env.PORT, host, port, () => {
-            console.log(`Server running at http://${host}:${port}/`)
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-    })
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database successfully");
+    app.listen(process.env.PORT, host, port, () => {
+      console.log(`Server running at http://${host}:${port}/`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies (as sent by API clients)
-app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies 
-app.use(express.static('public')); // Serve static files
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.static("public")); // Serve static files
 // For debugging purposes
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
+  console.log(req.path, req.method);
+  next();
 });
 
 // Routes
-app.use('/', mainRoutes);
-app.use('/api/exercises',exercisesRoutes);
-app.use('/api/users', usersRoutes);
-app.use('/api/posts', postsRoutes);
+app.use("/", mainRoutes);
+app.use("/api/exercises", exercisesRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/posts", postsRoutes);
+app.use("/api/workouts", workoutsRoutes);
