e69b865539d0bd0396d56c266c15f168057a90a5
FredSchuck <fredschuck@gmail.com>
2024-04-24 12:06:46 -0400

    Added states used for edit button and changed styling to create buttons

--- client/src/pages/ExerciseLibrary.jsx
+++ client/src/pages/ExerciseLibrary.jsx
@@ -6,6 +6,9 @@ const ExerciseLibrary = () => {
   useNavigate();
 
   const [workouts, setWorkouts] = useState([]);
+  const [exercises, setExercises] = useState([]);
+  const [selectedExercise, setSelectedExercise] = useState("");
+  const [selectedWorkout, setSelectedWorkout] = useState("");
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -20,8 +23,6 @@ const ExerciseLibrary = () => {
     fetchUserData();
   }, []);
 
-  const [exercises, setExercises] = useState([]);
-
   useEffect(() => {
     const fetchUserData = async () => {
       try {
@@ -35,6 +36,20 @@ const ExerciseLibrary = () => {
     fetchUserData();
   }, []);
 
+  const handleExerciseChange = (e) => {
+    setSelectedWorkout("");
+    setSelectedExercise(e.target.value);
+  };
+
+  const handleWorkoutChange = (e) => {
+    setSelectedExercise("");
+    setSelectedWorkout(e.target.value);
+  };
+
+  const handleEdit = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 flex justify-center">
       <div className="max-w-md w-full p-8 mx-4 bg-white rounded-lg shadow-md mb-auto mt-[100px]">
@@ -42,11 +57,18 @@ const ExerciseLibrary = () => {
 
         <h3>Workouts</h3>
         <form>
-          <select id="workouts" className="border rounded-lgblock w-full p-2.5">
-            <option selected>Choose a workout</option>
-            {workouts.map((e) => (
-              <option key={e._id} value={e.name}>
-                {e.name}
+          <select
+            id="workouts"
+            className="border rounded-lg block w-full p-2.5 mb-3"
+            onChange={handleWorkoutChange} // Corrected
+          >
+            <option value="">Choose a workout</option>
+            {workouts.map((workout) => (
+              <option
+                key={workout._id} // Removed unnecessary concatenation
+                value={workout.name}
+              >
+                {workout.name}
               </option>
             ))}
           </select>
@@ -56,21 +78,40 @@ const ExerciseLibrary = () => {
         <form>
           <select
             id="exercises"
-            className="border rounded-lgblock w-full p-2.5"
+            className="border rounded-lg block w-full p-2.5 mb-3"
+            onChange={handleExerciseChange} // Corrected
           >
-            <option selected>Choose an exercise</option>
-            {exercises.map((e) => (
-              <option key={e._id} value={e.name}>
-                {e.name}
+            <option value="">Choose an exercise</option>
+            {exercises.map((exercise) => (
+              <option
+                key={exercise._id} // Removed unnecessary concatenation
+                value={exercise.name}
+              >
+                {exercise.name}
               </option>
             ))}
           </select>
         </form>
 
-        <button type="submit" className="form-btn">
+        {selectedWorkout !== "" || selectedExercise !== "" ? (
+          <button type="submit" className="form-btn" onClick={handleEdit}>
+            Edit {selectedWorkout || selectedExercise}
+          </button>
+        ) : (
+          <button
+            type="submit"
+            className="form-btn-disabled"
+            onClick={handleEdit}
+            disabled
+          >
+            Edit
+          </button>
+        )}
+
+        <button type="submit" className="form-btn-outline">
           <Link to="/">Create New Workout</Link>
         </button>
-        <button type="submit" className="form-btn">
+        <button type="submit" className="form-btn-outline">
           <Link to="/create-exercise">Create New Exercise</Link>
         </button>
       </div>
