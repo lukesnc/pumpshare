61199cf1f0bac9f5ff2ca003526a55118af6b5df
FredSchuck <fredschuck@gmail.com>
2024-04-24 13:38:58 -0400

    Added icons and styling to them

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -40,6 +40,18 @@ const Navbar = () => {
         >
           PumpShare
         </Link>
+
+        <div className="my-auto ml-auto">
+          {user.email && (
+            <Link
+              to={`/log`}
+              className="my-auto ml-auto mr-[1em] text-white"
+              onClick={showMenu === "menu visible" ? toggleMenu : undefined}
+            >
+              <i className="fa-solid fa-plus fa-xl ml-2"></i>
+            </Link>
+          )}
+        </div>
         <div className="my-auto ml-auto">
           {user.email && (
             <Link
@@ -58,22 +70,44 @@ const Navbar = () => {
         {user.email ? (
           <>
             <NavLink to="/" className="nav-link" onClick={toggleMenu}>
-              Home
+              <div>
+                <i className="fa-solid fa-house mr-4 fa-xl menu-icon"></i>
+                Home
+              </div>
             </NavLink>
             <NavLink to="/dashboard" className="nav-link" onClick={toggleMenu}>
-              Dashboard
+              <div>
+                <i className="fa-solid fa-gauge mr-4 fa-xl menu-icon"></i>
+                Dashboard
+              </div>
             </NavLink>
             <NavLink to="/library" className="nav-link" onClick={toggleMenu}>
-              Exercise Library
+              <div>
+                <i className="fa-solid fa-book mr-4 fa-xl menu-icon"></i>
+                Exercise Library
+              </div>
             </NavLink>
             <NavLink to="/activity" className="nav-link" onClick={toggleMenu}>
-              Activity Feed
+              <div>
+                <i className="fa-solid fa-users-line mr-3 fa-xl menu-icon"></i>
+                Activity Feed
+              </div>
             </NavLink>
             <NavLink to="/settings" className="nav-link" onClick={toggleMenu}>
-              Settings
+              <div>
+                <i className="fa-solid fa-gears mr-3 fa-xl menu-icon"></i>
+                Settings
+              </div>
             </NavLink>
-            <NavLink to="/" className="nav-link" onClick={handleLogout}>
-              Logout
+            <NavLink
+              to="/"
+              className="nav-link text-[#C03939]"
+              onClick={handleLogout}
+            >
+              <div>
+                <i className="fa-solid fa-arrow-right-from-bracket mr-4 fa-xl opacity-70"></i>
+                Logout
+              </div>
             </NavLink>
           </>
         ) : (
