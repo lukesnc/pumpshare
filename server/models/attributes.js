27fb4fc71d0ea9a72100fd12e7d9eed18eb6ffed
FredSchuck <fredschuck@gmail.com>
2024-04-25 01:02:55 -0400

    Created attributes model

---
+++ server/models/attributes.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = require("mongoose").Schema;
+
+const attributeSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: 1,
+    },
+    unitType: {
+      type: String,
+      trim: true,
+    },
+    short: {
+      type: String,
+      required: [true, "Abbreviation of unit is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Attribute", attributeSchema, "attributes");
