4f6acfaefef78cace6cdeb474742646818b4044d
FredSchuck <fredschuck@gmail.com>
2024-04-25 01:04:31 -0400

    Added attributes route

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -5,16 +5,20 @@ const controller = require("../controllers/exercisesController");
 // GET all exercises
 router.get("/", controller.getAllExercises);
 
+router.get("/attributes", controller.getAllAttributes);
+
 // GET one exercise
-router.get('/:id', controller.getExercise);
+router.get("/:id", controller.getExercise);
 
 // POST one exercise
 router.post("/", controller.createExercise);
 
 router.post("/log", controller.logExercise);
 
+router.post("/attributes", controller.createNewAttribute);
+
 // DELETE one exercise
-router.delete('/:id', controller.deleteExercise);
+router.delete("/:id", controller.deleteExercise);
 
 // UPDATE one exercise
 router.patch("/:id", (req, res) => {
