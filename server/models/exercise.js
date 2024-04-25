ac1e1e6e121ff44e0390ceb5a2caee85fff7feaf
FredSchuck <fredschuck@gmail.com>
2024-04-25 01:04:16 -0400

    Removed unecessary fields in exercise schema

--- server/models/exercise.js
+++ server/models/exercise.js
@@ -1,20 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = require("mongoose").Schema;
 
-const exerciseSchema = new Schema({
-    name: { type: String, required: [true, 'Name is required'], trim: true, minlength: 1 },
-    attr: { type: String, required: [true, 'Attribute is required'], trim: true, minlength: 1 },
-    date: { type: Date, trim: true},
-    laps: { type: String, trim: true},
-    distance: {type: String, trim: true},
-    time: {type: String, trim: true},
-    sets: { type: String, trim: true},
-    reps: { type: String, trim: true},
-    weight: { type: String, trim: true},
-    about: { type: String, trim: true},
-    measurement: {type: String, trim: true},
-    timeUnit: {type: String, trim:true}
-    
-}, { timestamps: true });
+const exerciseSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: 1,
+    },
+    attr: [{ type: Schema.Types.ObjectId, ref: "Attribute" }],
+    date: { type: Date, trim: true },
+    about: { type: String, trim: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Exercise", exerciseSchema, "exercises");
