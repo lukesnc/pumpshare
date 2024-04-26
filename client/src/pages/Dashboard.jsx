0500a4ef9e68c4463a8af7b9cca0d04b01750779
SethSpoon <sethwspoon@outlook.com>
2024-04-26 14:01:56 -0400

    new dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,91 +1,41 @@
 import { features } from "../constants";
-import React from "react";
-import {
-  PieChart,
-  Pie,
-  Tooltip,
-  ResponsiveContainer,
-  BarChart,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Legend,
-  Bar,
-} from "recharts";
+import React from 'react';
+//import { PieChart, Pie, Tooltip, ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Legend, Bar } from 'recharts';
+import AreaChart from "../components/AreaChart";
+import BarChart from "../components/BarChart";
+import LineChart from "../components/LineChart";
+import PieChart from "../components/PieChart";
+import { Area } from "recharts";
 
-const Dashboard = () => {
-  const data = [
-    { name: "Exercise 1", value: 20000000 },
-    { name: "Exercise 2", value: 15000000 },
-    { name: "Exercise 3", value: 10000000 },
-    { name: "Exercise 4", value: 5000000 },
-  ];
 
-  const gridContainerStyle = {
-    display: "grid",
-    gridTemplateColumns: "repeat(2, 1fr)",
-    gap: "20px",
-    justifyContent: "center", // horizontally center the items
-    alignItems: "center", // vertically center the items
-  };
 
-  const chartContainerStyle = {
-    border: "1px solid #ccc",
-    borderRadius: "5px",
-    padding: "20px",
-  };
+export default function Dashboard() {
+    return(
+        <main className="flex min-h-screen flex-col item-center justify-center px-4 md:px-8 xl:px-10 py-44">
+        <div className="grid xl:grid-cols-3 lg:grid-cols-2 w-full gap-10 max-w-[1400px]">
+        <GridItem title="Area Chart">
+            <AreaChart />
+        </GridItem>
 
-  return (
-    <div className="flex flex-col">
-      <div className="">
-        <div className="flex flex-col mt-[5em]" style={chartContainerStyle}>
-          <h2>Pie Chart</h2>
-          <ResponsiveContainer width="100%" height={300}>
-            <PieChart>
-              <Pie
-                dataKey="value"
-                isAnimationActive={false}
-                data={data}
-                cx="50%"
-                cy="50%"
-                outerRadius={80}
-                fill="#8884d8"
-                label
-              />
-              <Tooltip />
-            </PieChart>
-          </ResponsiveContainer>
+
+        <GridItem title="Bar Chart">
+            <BarChart />
+        </GridItem>
+
+
+        <GridItem title="Line Chart">
+            <LineChart />
+        </GridItem>
+
+        <GridItem title="Pie Chart">
+            <PieChart />
+        </GridItem>
+
+
         </div>
-
-        <div className="flex flex-col" style={chartContainerStyle}>
-          <h2>Bar Chart</h2>
-          <ResponsiveContainer width="100%" height={200}>
-            <BarChart
-              width={500}
-              height={300}
-              data={data}
-              margin={{
-                top: 20,
-                right: 30,
-                left: 40,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="value" stackId="a" fill="#8884d8" />
-              <Bar dataKey="value" stackId="a" fill="#82ca9d" />
-              <Bar dataKey="value" fill="#ffc658" />
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
-      </div>
-    </div>
-  );
-};
+        </main>
+    );
+}
 
 function GridItem({title, children}){
     return (
