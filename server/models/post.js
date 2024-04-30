f0faf207a58c01a1de00d1bdb4b40a4d477495a5
lukesnc <lukeasimone@gmail.com>
2024-04-30 10:46:27 -0400

    formatting

--- server/models/post.js
+++ server/models/post.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose');
-const Schema = require('mongoose').Schema;
+const mongoose = require("mongoose");
+const Schema = require("mongoose").Schema;
 
-const postSchema = new Schema({
-    content: { type: String, required: [true, 'Content is required'], trim: true, minlength: 1 },
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
-}, { timestamps: true });
+const postSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: [true, "Content is required"],
+      trim: true,
+      minlength: 1,
+    },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
 
-
-module.exports = mongoose.model('Post', postSchema, 'posts');
+module.exports = mongoose.model("Post", postSchema, "posts");
