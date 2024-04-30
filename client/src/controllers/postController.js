2640bc21ce78175473f427e9e28954f58938f504
FredSchuck <fredschuck@gmail.com>
2024-04-30 10:01:56 -0400

    Completed createPost function

--- client/src/controllers/postController.js
+++ client/src/controllers/postController.js
@@ -1,31 +1,19 @@
-import {getMe} from "./userController"
+import { getMe } from "./userController";
 
 const createPost = async (content) => {
-    // const content = content;
-    console.log("content: ", content)
-    const token = localStorage.getItem("token");
-    if (!token) {
-        throw Error("No token found");
-    }
-    const user = await getMe();
-    // userId = user._id;
-    console.log("user", user)
-    
-    console.log("hahxhshshshshs-2")
-    console.log("this is before we fetch")
-    const res = await fetch("/api/posts/create", {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    throw Error("No token found");
+  }
+  const res = await fetch("/api/posts/create", {
     method: "POST",
     headers: {
-        // Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`, // Issue here
+      "Content-Type": "application/json",
     },
-    // body: JSON.stringify({ content, userId })
-    });
-    console.log("we're back!! ", res)
-    const data = await res.json();
-    return res; 
-}
+    body: JSON.stringify({ content }),
+  });
+  return res;
+};
 
-export {
-    createPost
-  };
+export { createPost };
