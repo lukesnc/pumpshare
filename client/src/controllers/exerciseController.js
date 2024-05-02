e23b74e4f69897d112a96428b8d2f003a2ac8593
FredSchuck <fredschuck@gmail.com>
2024-05-02 17:26:27 -0400

    Pretty automatic syntax change

--- client/src/controllers/exerciseController.js
+++ client/src/controllers/exerciseController.js
@@ -3,63 +3,57 @@ const createExercise = async (name, attr) => {
     throw Error("All fields are required");
   }
 
-    const res = await fetch('/api/exercises/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, attr }),
-    });
-    
-    // data is the response from the server
-    const data = await res.json();
-    if (!res.ok) {
-        throw Error(data.error);
-    }
-    return data; 
+  const res = await fetch("/api/exercises/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, attr }),
+  });
+
+  const data = await res.json();
+  if (!res.ok) {
+    throw Error(data.error);
+  }
+  return data;
 };
 
-const getExercise = async (id) =>{
-    console.log(id.logId);
-    const res = await fetch('/api/exercises/' + id.logId , {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        
-    });
-    const data = await res.json();
-    
-    return data;
-};
-
-const logExercise = async (exercise,about, values) =>{
-    const res = await fetch('/api/exercises/log', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({exercise, about, values }),
-    });
-    
-    // data is the response from the server
-    const data = await res.json();
-    if (!res.ok) {
-        throw Error(data.error);
-    }
+const getExercise = async (id) => {
+  const res = await fetch("/api/exercises/" + id.logId, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
 
   return data;
 };
 
-const deleteExercise = async(exercise) =>{
+const logExercise = async (exercise, about, values) => {
+  const res = await fetch("/api/exercises/log", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ exercise, about, values }),
+  });
 
-    let id = exercise.exercise._id;
-    const res = await fetch('/api/exercises/' + id , {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        
-    });
-}
-export { createExercise, logExercise, getExercise, deleteExercise }
+  const data = await res.json();
+  if (!res.ok) {
+    throw Error(data.error);
+  }
+
+  return data;
+};
+
+const deleteExercise = async (exercise) => {
+  let id = exercise.exercise._id;
+  const res = await fetch("/api/exercises/" + id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+export { createExercise, logExercise, getExercise, deleteExercise };
